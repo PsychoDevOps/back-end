c24f2658d774dba6ba3ce75cb1ec1ac745ef1774
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-05-02 07:41:37 -0500

    update cors

--- src/main/java/com/psycho/psychohelp/PsychohelpApiApplication.java
+++ src/main/java/com/psycho/psychohelp/PsychohelpApiApplication.java
@@ -17,24 +17,14 @@ public class PsychohelpApiApplication {
         SpringApplication.run(PsychohelpApiApplication.class, args);
     }
 
-    @Bean
+   @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/**").allowedOrigins("https://psychohelp.azurewebsites.net").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("https://psychohelp.azurewebsites.net").allowedMethods("*").allowedHeaders("*");
             }
-        };
+             };
     }
-
-  //  @Bean
-    //public WebMvcConfigurer corsConfigurer() {
-      //  return new WebMvcConfigurer() {
-        //    @Override
-          //  public void addCorsMappings(CorsRegistry registry) {
-            //    registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("*").allowedHeaders("*");
-            //}
-       // };
-    //}
 
 }
