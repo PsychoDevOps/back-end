ca6903597847587b6bbbc13c7c393dc4c386a14f
VivianMPQ <104032517+VivianMPQ@users.noreply.github.com>
2023-05-04 19:23:39 -0500

    feat: test psicologo buscar por nombre

--- src/test/java/com/psycho/psychohelp/psychologist/service/PsychologistServiceTest.java
+++ src/test/java/com/psycho/psychohelp/psychologist/service/PsychologistServiceTest.java
@@ -117,7 +117,54 @@ public class PsychologistServiceTest {
 
  }
 
+ @Test
+ @DisplayName("test buscar psicologo por genero y tipo de sesion")
+ public void testBuscarPorGeneroYTipoDeSesion(){
+  ArrayList<Psychologist> espero = new ArrayList<>();
+  espero.add(new Psychologist(1L, "Jorge",  "12345","1980/12/05","jorgel@mail.com","password",
+          "984561278","especializacion","formacion","about", "masculino","sesiontype", "img.jpg","cmp",true,true));
 
+  espero.add(new Psychologist(2L, "Jorgito",  "123456","1980/12/05","jorgitol@mail.com","password",
+          "984568278","especializacion","formacion","about", "masculino","sesiontype1", "img.jpg","cmp",true,true));
+
+  List<Psychologist> psychologists = new ArrayList<>();
+  psychologists.add(new Psychologist(1L, "Jorge",  "12345","1980/12/05","jorgel@mail.com","password",
+          "984561278","especializacion","formacion","about", "masculino","sesiontype", "img.jpg","cmp",true,true));
+
+  psychologists.add(new Psychologist(2L, "Jorgito",  "123456","1980/12/05","jorgitol@mail.com","password",
+          "984568278","especializacion","formacion","about", "masculino","sesiontype1", "img.jpg","cmp",true,true));
+
+
+  Mockito.when(psychologistRepository.findByGenreAndSessionType("masculino","sesiontype"))
+          .thenReturn(psychologists);
+
+
+  List<Psychologist> recibo = psychologistService.getByGenreAndSessionType("masculino","sesiontype");
+  Assertions.assertEquals(espero.get(0).getGenre(),recibo.get(0).getGenre());
+  Assertions.assertEquals(espero.get(1).getGenre(),recibo.get(1).getGenre());
+  Assertions.assertEquals(espero.get(0).getSessionType(),recibo.get(0).getSessionType());
+  Assertions.assertEquals(espero.get(1).getSessionType(),recibo.get(1).getSessionType());
+  Mockito.verify(psychologistRepository,Mockito.times(1)).findByGenreAndSessionType(Mockito.anyString(),Mockito.anyString());
+
+ }
+
+
+ @Test
+ @DisplayName("test buscar psicologo por nombre")
+ public void testBuscarPorNombre(){
+  Psychologist espero = new Psychologist(1L, "Jorge",  "12345","1980/12/05","jorgel@mail.com","password",
+          "984561278","especializacion","formacion","about", "masculino","sesiontype", "img.jpg","cmp",true,true);
+
+  Mockito.when(psychologistRepository.findByName(Mockito.anyString()))
+          .thenReturn(new Psychologist(1L, "Jorge", "12345", "1980/12/05", "jorgel@mail.com", "password",
+                  "984561278", "especializacion", "formacion", "about", "masculino", "sesiontype", "img.jpg", "cmp", true, true));
+
+  Psychologist recibo =psychologistService.getByName("Jorge");
+  Assertions.assertEquals(espero.getName(),recibo.getName());
+
+  Mockito.verify(psychologistRepository,Mockito.times(1)).findByName(Mockito.anyString());
+
+ }
 
 
 
