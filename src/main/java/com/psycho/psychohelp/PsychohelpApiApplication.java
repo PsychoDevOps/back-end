1381ab6bad6a4ccd58844e64ca40401b2a8ddf5f
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-06-06 14:13:40 -0500

    Change WAR package

--- src/main/java/com/psycho/psychohelp/PsychohelpApiApplication.java
+++ src/main/java/com/psycho/psychohelp/PsychohelpApiApplication.java
@@ -3,6 +3,7 @@ package com.psycho.psychohelp;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -16,7 +17,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @OpenAPIDefinition
 @SpringBootApplication
 @EnableJpaAuditing
-public class PsychohelpApiApplication {
+public class PsychohelpApiApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(PsychohelpApiApplication.class, args);
--- src/main/java/com/psycho/psychohelp/psychologist/api/PsychologistController.java
+++ src/main/java/com/psycho/psychohelp/psychologist/api/PsychologistController.java
@@ -40,7 +40,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Psychologist")
 @RestController
 @RequestMapping("/api/v1/psychologists")
-@CrossOrigin
 public class PsychologistController {
 
   @Autowired
